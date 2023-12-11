d69da1ad2f1d0504fb66d8189b625024a8d51f44
NickSot <nikola.s.sotirov@gmail.com>
2023-12-11 15:28:26 +0200

    add members' images

--- src/modules/request_handler.js
+++ src/modules/request_handler.js
@@ -4,15 +4,33 @@ endpoints =  require('./endpoints.js');
 const handleRequest = (request, response) => {
 	if (request.method == "GET") {
 		switch (request.url) {
-			case '/':
-				endpoints.resource(response, './static/index.html', 'text/html');
-				break;
+			// resources
 			case '/static/styles.css':
 				endpoints.resource(response, './static/styles.css', 'text/css');
 				break;
 			case '/static/logo.png':
 				endpoints.resource(response, './static/logo.png', 'image/png');
 				break;
+			case '/static/ivo.jpg':
+				endpoints.resource(response, './static/ivo.jpg', 'image/jpg');
+				break;
+			case '/static/mitko.jpg':
+				endpoints.resource(response, './static/mitko.jpg', 'image/jpg');
+				break;
+			case '/static/nedi.jpg':
+				endpoints.resource(response, './static/nedi.jpg', 'image/jpg');
+				break;
+			case '/static/yasen.jpg':
+				endpoints.resource(response, './static/yasen.jpg', 'image/jpg');
+				break;
+			case '/static/pesho.jpg':
+				endpoints.resource(response, './static/pesho.jpg', 'image/jpg');
+				break;
+
+			// pages
+			case '/':
+				endpoints.resource(response, './static/index.html', 'text/html');
+				break;			
 			case '/about':
 				endpoints.resource(response, './static/about.html', 'text/html');
 				break;
